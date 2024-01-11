dc86346a2c9d85259ee209e88b8e22acde2cad7b
Kalpan22 <kalpanpatel2299@gmail.com>
2024-01-11 12:36:23 +0530

    complete code

--- Calcu.java
+++ Calcu.java
@@ -1,7 +1,7 @@
 interface calcu
 {
 public int sum(int a,int b);
-public int diffsum(int a.int b);
+public int diff(int a.int b);
 }
 class calcuImpl implements calcu
 {
@@ -9,9 +9,24 @@ class calcuImpl implements calcu
     {
         return a+b;
     }
-    public int diffSum(int a,int b)
+    public int diff(int a,int b)
     {
         return a-b;
     }
 
+}
+import java.util.Scanner;
+class calcuDriver
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        calcuImpl c=new calcuImpl();
+        System.out.println("Enter 1st number");
+        int n1=s.nextInt();
+        System.out.println("Enter 2nd number");
+        int n2=s.nextInt();
+        System.out.println(n1+"+"+n2+"="+c.sum(n1,n2));
+        System.out.println(n1+"-"+n2+"="+c.diff(n1,n2));
+    }
 }
